27336e7867ca44d31cf3cf24f8f88e8fd29354d9
farandole77 <liechan77@gmail.com>
2025-07-09 19:59:52 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,16 @@
-+16
--16
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// src/firebase.js␊
-import { initializeApp } from "firebase/app";␊
-import { getAuth } from "firebase/auth";␊
-import { getFirestore } from "firebase/firestore";␊
-␊
-const firebaseConfig = {␊
-  apiKey: "AIzaSyD1MzFkoOcvBHiKhm9ii-XbTtJns6VlLno",␊
-  authDomain: "duncantoystore-f5a3d.firebaseapp.com",␊
-  projectId: "duncantoystore-f5a3d",␊
-  storageBucket: "duncantoystore-f5a3d.firebasestorage.app",␊
-  messagingSenderId: "712063855287",␊
-  appId: "1:712063855287:web:26f425cf54e50418b93b32",␊
-};␊
-␊
-const app = initializeApp(firebaseConfig);␊
-export const auth = getAuth(app);␊
-export const db = getFirestore(app);␊
+const firebaseConfig = {
+  apiKey: "AIzaSyD1MzFkoOcvBHiKhm9ii-XbTtJns6VlLno",
+  authDomain: "duncantoystore-f5a3d.firebaseapp.com",
+  projectId: "duncantoystore-f5a3d",
+  storageBucket: "duncantoystore-f5a3d.firebasestorage.app",
+  messagingSenderId: "712063855287",
+  appId: "1:712063855287:web:26f425cf54e50418b93b32",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
